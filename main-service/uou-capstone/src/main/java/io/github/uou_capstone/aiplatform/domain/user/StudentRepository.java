5291ec9299f8eb99ed43cca9f896d9f320d3b57a
hanmogo <russell1677@naver.com>
2025-10-09 17:49:16 +0900

    feat: user 도메인 구현

---
+++ main-service/uou-capstone/src/main/java/io/github/uou_capstone/aiplatform/domain/user/StudentRepository.java
@@ -0,0 +1,8 @@
+package io.github.uou_capstone.aiplatform.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+}
